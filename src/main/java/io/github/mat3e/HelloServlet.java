c1a42c6cbcf4cd39c1ffb86733649f73516945c1
Maks <mxlazarewicz@gmail.com>
2021-08-16 21:37:10 +0200

    Request with parameter, getting parameter value and Writing hello + param or if Param is null, say Hello World

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -9,15 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "Hello", urlPatterns = {"api/*"})
 public class HelloServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+    private static final String NAME_PARAM = "name" ;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        logger.info("Request got");
-        resp.getWriter().write("Hello World");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Got request with parameters" + req.getParameterMap());
+       var name = Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("world");
+
+       resp.getWriter().write("Hello " + name + "!");
+
+
     }
+
 
 
 

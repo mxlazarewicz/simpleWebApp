2272f747951b00dc6c12fb6cdc46022db18cbfcb
Maks <mxlazarewicz@gmail.com>
2021-08-17 21:16:56 +0200

    Add Lang class, add Lang repository - finding language by Id method

---
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -0,0 +1,20 @@
+package io.github.mat3e;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class LangRepository {
+    private List<Lang> languages;
+
+    LangRepository(){
+        languages = new ArrayList<>();
+        languages.add(new Lang(1L, "Hello", "en"));
+        languages.add(new Lang(2L, "Witam", "pn"));
+    }
+  Optional<Lang> findById(Long id){
+        return languages.stream()
+                .filter(l -> l.getId()
+                        .equals(id)).findFirst();
+    }
+}

27691e418561fa1552faeda5b4a6583a2f21724f
Maks <mxlazarewicz@gmail.com>
2021-08-18 09:10:38 +0200

    Modified Servlet and Service, tests are ok

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -4,9 +4,28 @@ import java.util.Optional;
 
 class HelloService {
     static final String FALLBACK_NAME = "world";
+    static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
+
+    private LangRepository repository;
+
+    HelloService(){
+        this(new LangRepository());
+    }
+
+    public HelloService(LangRepository repository) {
+    this.repository = repository;
+
+    }
+
 
     String prepareGreeeting(String name) {
-       return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
+     return   prepareGreeeting(name, null);
+    }
+    String prepareGreeeting(String name, String lang){
+        var langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        var welcomeMsg = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
+        var nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return welcomeMsg + " " + nameToWelcome + "!";
     }
 
 }
--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 public class HelloServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
     private static final String NAME_PARAM = "name" ;
+    private static final String LANG_PARAM = "lang" ;
     private HelloService service;
 
     /**
@@ -33,7 +34,9 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("Got request with parameters" + req.getParameterMap());
-        resp.getWriter().write(service.prepareGreeeting(req.getParameter(NAME_PARAM)));
+        var name = req.getParameter(NAME_PARAM);
+        var lang = req.getParameter(LANG_PARAM);
+        resp.getWriter().write(service.prepareGreeeting(name, lang));
 
 
     }

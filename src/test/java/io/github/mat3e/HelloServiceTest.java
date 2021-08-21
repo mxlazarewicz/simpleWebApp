7b195cf390024d47b4e1cbff84b8b1d642727c15
Maks <mxlazarewicz@gmail.com>
2021-08-21 20:19:57 +0200

    Conncting to database h2 with flyway, creating table languages, failed to second migrate

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -55,6 +55,24 @@ public class HelloServiceTest {
         assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
+    @Test
+    public void  test_prepareGreeting_non_Existing_repository() throws Exception {
+        //given
+
+        var mockRepository = new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.empty();
+            }
+        };
+        var SUT = new HelloService(mockRepository);
+        //when
+        var result = SUT.prepareGreeeting(null, "-1");
+        //then
+        assertEquals(HelloService.FALLBACK_LANG.getWelcomeMsg() + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+
     private LangRepository fallbackLangIDRepository() {
         return new LangRepository(){
             @Override
